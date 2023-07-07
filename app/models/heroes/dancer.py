85d44a42acbc4ff43ddbfa2075b3fac8e4a03142
effervescent000 <kislio01@gmail.com>
2023-07-07 06:50:35 -0400

    simple test

--- app/models/heroes/dancer.py
+++ app/models/heroes/dancer.py
@@ -3,5 +3,5 @@ from app.models.base_hero import BaseHero
 
 class Dancer(BaseHero):
     @classmethod
-    def create_new(cls) -> Dancer:
+    def create_new(cls) -> "Dancer":
         return cls(name="test", level=1, max_health=100, max_mana=100)
--- app/routers/recruit.py
+++ app/routers/recruit.py
@@ -1,11 +1,14 @@
+from typing import Sequence
+
 from fastapi import APIRouter
 
+from app.models.base_hero import BaseHero
 from app.models.heroes.dancer import Dancer
 
 router = APIRouter(prefix="/recruit")
 
 
 @router.get("/")
-async def generate_recruits():
+async def generate_recruits() -> Sequence[BaseHero]:
     heroes = [Dancer.create_new()]
     return heroes
