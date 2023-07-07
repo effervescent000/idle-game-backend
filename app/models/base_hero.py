ae278220b819d64be909912466fc5c157d146c4b
effervescent000 <kislio01@gmail.com>
2023-07-07 10:43:27 -0400

    add abilities

--- app/models/base_hero.py
+++ app/models/base_hero.py
@@ -1,8 +1,24 @@
 from __future__ import annotations
 
-from typing import Mapping
-
 from pydantic import BaseModel
+
+
+class Stat(BaseModel):
+    name: str
+    amount: int
+
+
+class Ability(BaseModel):
+    name: str
+    rank: int = 1
+    execution_time: float = 0
+    cooldown: float = 0
+    base_damage: int
+    damage_coefficient: float
+
+    @classmethod
+    def make_standard_attack(cls, *, base_damage: int) -> "Ability":
+        return cls(name="Attack", base_damage=base_damage, damage_coefficient=0.5)
 
 
 class BaseHero(BaseModel):
@@ -10,6 +26,10 @@ class BaseHero(BaseModel):
     level: int
     max_health: int
     max_mana: int
+    # XXX eventually we'll get rid of this stats field
+    # and derive stats based on gear
+    stats: list[Stat] = []
+    attacks: list[Ability] = []
 
     @classmethod
     def create_new(cls) -> "BaseHero":
--- app/models/heroes/dancer.py
+++ app/models/heroes/dancer.py
@@ -1,11 +1,13 @@
-from typing import Mapping
-
-from app.utils.utils import wrapData
-
-from app.models.base_hero import BaseHero
+from app.models.base_hero import BaseHero, Ability
 
 
 class Dancer(BaseHero):
     @classmethod
     def create_new(cls) -> "Dancer":
-        return cls(name="test", level=1, max_health=100, max_mana=100)
+        return cls(
+            name="test",
+            level=1,
+            max_health=10,
+            max_mana=10,
+            attacks=[Ability.make_standard_attack(base_damage=5)],
+        )
